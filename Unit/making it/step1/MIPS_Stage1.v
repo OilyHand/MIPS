c524bdc5b947ef9089357d2f6530b2d9954d5540
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-11-27 23:53:33 +0900

    Update MIPS_Stage1.v

--- Unit/making it/step1/MIPS_Stage1.v
+++ Unit/making it/step1/MIPS_Stage1.v
@@ -1,26 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/11/27 23:04:58
+// Design Name: 
+// Module Name: MIPS_Stage1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module MIPS_Stage1(
     input wire clk,
     input wire rst,
-    output reg [31:0] pc,
-    output reg [31:0] inst  // 현재 명령어
+    output reg [31:0] pc,   // PC
+    output reg [31:0] inst  // instruction
 );
+reg  [31:0] pc_inner; //register를 바로 output에 연결 할 수 없기에 새로운 변수 생성
 
   
     ProgramCounter U0 (
         .clk(clk),
+        
         .branchAddress(0),
         .branch(0),
         .jumpAddress(0),
         .jump(0),
         .PCWriteValue(0),
-        .PCWrite(1),  // PC 값
-        .pc(pc)
+        .PCWrite(1),
+        
+        .pc(pc_inner)
     );
 
   
     InstructionMemory U1 (
         .clk(clk),
-        .address(pc),
+        .address(pc_inner),
         .instruction(inst)
     );
 
@@ -31,10 +56,8 @@ module MIPS_Stage1(
             pc <= 32'h0;
         end else begin
             // 리셋이 아니면 PC 값을 업데이트
-            pc <= U0.pc;
+            pc <= U0.pc_inner; //이제 wire pc에 값을 주고 output으로 갈 수 있게 함.
         end
     end
 
 endmodule
-
-
