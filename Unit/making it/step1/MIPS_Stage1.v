c57cbdc300cff2f16d80d1474b458b87020f81ac
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-11-28 01:39:58 +0900

    Update MIPS_Stage1.v

--- Unit/making it/step1/MIPS_Stage1.v
+++ Unit/making it/step1/MIPS_Stage1.v
@@ -21,14 +21,37 @@
 
 // synthesis, Implementation 통과
 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/11/27 23:04:58
+// Design Name: 
+// Module Name: MIPS_Stage1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// synthesis, Implementation 통과
+
 module MIPS_Stage1(
     input wire clk,
     input wire rst,
-    output reg [31:0] pc,
+    output wire [31:0] pc,
     output wire [31:0] inst
 );
-
-    wire [31:0] pc_internal; // 중간 변수 추가
+    reg [31:0] pc_reg; // 중간 변수 추가
+    wire [31:0] add_result;
 
     ProgramCounter U0 (
         .clk(clk),
@@ -38,23 +61,33 @@ module MIPS_Stage1(
         .jump(0),
         .PCWriteValue(0), // pc를 control과 연결하지 않을 경우 빼기
         .PCWrite(1),
-        .pc(pc_internal) // 중간 변수에 연결
+        .pc(pc) // 중간 변수에 연결
+    );
+
+    Adder U2 (
+        .operandA(pc),
+        .operandB(4),
+        .sum(add_result)
     );
 
     InstructionMemory U1 (
         .clk(clk),
-        .address(pc_internal),
+        .address(pc),
         .instruction(inst)
     );
+    
 
     always @(posedge clk or posedge rst) begin // negedge rst 사용하는 것도 고려해보기
         if (rst) begin
             // 리셋 신호가 활성화되면 초기값으로 리셋
-            pc <= 32'h0;
+            pc_reg <= 32'h0;
         end else begin
             // 리셋이 아니면 PC 값을 업데이트
-            pc <= U0.pc;
+            pc_reg <= U2.sum;
         end
     end
+    
+    assign pc = pc_reg;
+
 
 endmodule
