9e68a3302a8bca0e43ef4ea4aa04fd772bb8b338
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-11-28 00:24:05 +0900

    Update MIPS_Stage1.v

--- Unit/making it/step1/MIPS_Stage1.v
+++ Unit/making it/step1/MIPS_Stage1.v
@@ -23,32 +23,28 @@
 module MIPS_Stage1(
     input wire clk,
     input wire rst,
-    output reg [31:0] pc,   // PC
-    output reg [31:0] inst  // instruction
+    output reg [31:0] pc,
+    output wire [31:0] inst
 );
-    reg  [31:0] pc_inner; // register를 바로 output에 연결 할 수 없기에 새로운 변수 생성
 
-  
+    wire [31:0] pc_internal; // 중간 변수 추가
+
     ProgramCounter U0 (
         .clk(clk),
-        
         .branchAddress(0),
         .branch(0),
         .jumpAddress(0),
         .jump(0),
         .PCWriteValue(0),
         .PCWrite(1),
-        
-        .pc(pc_inner)
+        .pc(pc_internal) // 중간 변수에 연결
     );
 
-  
     InstructionMemory U1 (
         .clk(clk),
-        .address(pc_inner),
+        .address(pc_internal),
         .instruction(inst)
     );
-
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
@@ -56,8 +52,7 @@ module MIPS_Stage1(
             pc <= 32'h0;
         end else begin
             // 리셋이 아니면 PC 값을 업데이트
-            pc <= U0.pc_inner;
-            // 이제 wire pc에 값을 주고 output으로 갈 수 있게 함.
+            pc <= U0.pc;
         end
     end
 
