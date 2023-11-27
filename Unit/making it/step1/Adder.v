0b79ade02958c31d4ccbcfeb60e1724720469593
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-11-28 03:11:36 +0900

    Update Adder.v

--- Unit/making it/step1/Adder.v
+++ Unit/making it/step1/Adder.v
@@ -1,12 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/11/28 00:47:03
+// Design Name: 
+// Module Name: Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module Adder(
     input wire [31:0] operandA, // 피연산자 A
     input wire [31:0] operandB, // 피연산자 B
-    output reg [31:0] sum       // 덧셈 결과
+    output wire [31:0] sum       // 덧셈 결과
 );
 
-    always @* begin
-        // 두 피연산자를 더하기
-        sum = operandA + operandB;
-    end
+    assign sum = operandA + operandB;
 
 endmodule
