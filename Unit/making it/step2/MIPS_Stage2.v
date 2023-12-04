b37ba4beea89e8154ac11d811de7d2c7daba8f26
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-04 19:27:56 +0900

    Update MIPS_Stage2.v

--- Unit/making it/step2/MIPS_Stage2.v
+++ Unit/making it/step2/MIPS_Stage2.v
@@ -20,17 +20,23 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module MIPS_Stage2(
-    input wire clk,
-    input wire rst,
-    input wire [31:0] pc,
-    input wire [31:0] inst,
-    output wire [31:0] readData1,
-    output wire [31:0] readData2,
-    output wire [31:0] signExtended,
-    output wire [4:0] writeReg,
-    output wire [31:0] writeData,
-    output wire RegWrite
+module ID_Stage(
+    input wire IFtoID_clk,
+    input wire IFtoID_rst,
+    input wire [31:0] IFtoID_PC,
+    input wire [31:0] IFtoID_inst,
+    
+    output wire [31:0] IDtoEX_PC,
+    output wire [31:0] IDtoEX_ReadData1,
+    output wire [31:0] IDtoEX_ReadData2,
+    output wire [31:0] IDtoEX_Imm,
+    output wire [5:0] IFtoID_Rs,    //25:21
+    output wire [5:0] IFtoID_Rt,    //20:16
+    output wire [5:0] IFtoID_Rd     //15:11
+
+/*    input wire [4:0] writeReg,
+    input wire [31:0] writeData,
+    input wire RegWrite */
 );
 
     // Register File
@@ -38,8 +44,26 @@ module MIPS_Stage2(
     wire [4:0] RR1, RR2, WR;
     wire [31:0] WD;
     wire WriteReg;
+    wire [31:0] imm; // using sign extension
+    wire ALUSrc, RegDst, Branch, MemRead, Memwrite, RegWrite, RegWrite, MemtoReg, PCSrc;
+    wire [1:0] ALUOp;
     
-    RegisterFile U0 (
+    Control u0 (
+        .hazard_detected(/*stall*/),
+        .opcode(IFtoID_inst[31:26]),
+        .ALUSrc(ALUSrc),
+        .RegDst(RegDst),
+        .ALUOp(ALUOp),
+        .Branch(Branch),
+        .MemRead(MemRead),
+        .MemWrite(MemWrite),
+        .RegWrite(RegWrite),
+        .MemtoReg(MemtoReg),
+        .PCSrc(PCSrc)
+    );
+    
+    
+    RegisterFile u1 (
         .RD1(RD1),
         .RD2(RD2),
         .RR1(RR1),
@@ -51,29 +75,33 @@ module MIPS_Stage2(
     );
 
 
-    // Sign Extension
-    wire [31:0] imm;
-    SignExt U1 (
+    SignExt u2 (
         .Y(imm),
-        .X(inst[15:0])
+        .X(IFtoID_inst[15:0])
     );
 
 
     // Instruction Decode
-    assign RR1 = inst[25:21];
-    assign RR2 = inst[20:16];
-    assign WR = inst[15:11];
-    assign WriteReg = (inst[31:26] == 6'b00000) ? 0 : 1; // R-type일 때 WriteReg 활성화
+    assign RR1 = IFtoID_inst[25:21];
+    assign RR2 = IFtoID_inst[20:16];
+    assign WR = IFtoID_inst[15:11];
+    assign WriteReg = (IFtoID_inst[31:26] == 6'b00000) ? 0 : 1; // R-type일 때 WriteReg 활성화
+
 
     // Mux for Write Data
-    assign WD = (inst[31:26] == 6'b00000) ? readData2 : imm;
+    assign WD = (IFtoID_inst[31:26] == 6'b00000) ? IDtoEX_ReadData2 : imm;
+
 
     // Output
-    assign readData1 = RD1;
-    assign readData2 = RD2;
-    assign signExtended = imm;
-    assign writeReg = WR;
+    assign IDtoEX_PC = IFtoID_PC;
+    assign IDtoEX_ReadData1 = RD1;
+    assign IDtoEX_ReadData2 = RD2;
+    assign IDtoEX_imm = imm;
+    assign IFtoID_Rs = IFtoID_inst[25:21];
+    assign IFtoID_Rt = IFtoID_inst[20:16];
+    assign IFtoID_Rd = IFtoID_inst[15:11];
+/*    assign writeReg = WR;
     assign writeData = WD;
     assign RegWrite = WriteReg;
-
+*/
 endmodule
