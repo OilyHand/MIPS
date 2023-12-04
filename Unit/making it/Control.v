97a6917d66e62f777e366e9a0d9ff08633a3e144
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-04 12:50:51 +0900

    Update Control.v

--- Unit/making it/Control.v
+++ Unit/making it/Control.v
@@ -23,22 +23,24 @@
 module Control(
     input wire hazard_detected,
     input wire [5:0] opcode,
-    output reg ALUSrc, RegDst,ALUOp1, ALUOp2, // stage 3에서 사용
+    output reg ALUSrc, RegDst,
+    output reg [1:0] ALUOp, // stage 3에서 사용
     output reg Branch, MemRead, MemWrite,     // stage 4에서 사용
     output reg RegWrite, MemtoReg, PCSrc);    // stage 5 이후에서 사용
 
 
 always @* begin
     if (hazard_detected == 0) begin
-        {ALUSrc, RegDst, ALUOp1, ALUOp2, Branch, MemRead, MemWrite, RegWrite, MemtoReg, PCSrc} <= 0;
+        {ALUSrc, RegDst, ALUOp, Branch, MemRead, MemWrite, RegWrite, MemtoReg, PCSrc} <= 0;
         if (opcode == 6'b100011) begin // LW instruction
-            {ALUSrc, MemRead, RegWrite, MemtoReg} = 1; end
+            {ALUSrc, MemRead, RegWrite, MemtoReg} <= 1; end
         else if (opcode == 6'b101011) begin // SW instrtuction
-            {ALUSrc, MemWrite} = 1; end
+            {ALUSrc, MemWrite} <= 1; end
         else if (opcode == 6'b000100) begin // BEQ instruction
-            {ALUOp2, Branch} = 1; end
+            {ALUOp, Branch} <= 1; end
         else if (opcode == 6'b000000) begin // R-type instruction
-            {RegDst, ALUOp1, RegWrite} = 1; end
+             ALUOp <= 2;
+            {RegDst, RegWrite} <= 1; end
     end
 end
 endmodule
