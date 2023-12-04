212a0928bf98f62ea1b25fcf943eafb21b7d94bf
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-04 15:23:49 +0900

    Update Control.v

--- Unit/making it/step2/Control.v
+++ Unit/making it/step2/Control.v
@@ -24,7 +24,7 @@ module Control(
     input wire hazard_detected,
     input wire [5:0] opcode,
     output reg ALUSrc, RegDst,
-    output reg [1:0] ALUOp, // stage 3에서 사용
+    output reg [1:0] ALUOp,                   // stage 3에서 사용
     output reg Branch, MemRead, MemWrite,     // stage 4에서 사용
     output reg RegWrite, MemtoReg, PCSrc);    // stage 5 이후에서 사용
 
@@ -41,6 +41,9 @@ always @* begin
         else if (opcode == 6'b000000) begin // R-type instruction
              ALUOp <= 2;
             {RegDst, RegWrite} <= 1; end
+    end
+    else begin
+    
     end
 end
 endmodule
