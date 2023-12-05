694a9dec0b8d1067ecdb3febdf7352856720b348
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-05 16:01:35 +0900

    Update IF_Stage.v

--- Unit/making it/step1/IF_Stage.v
+++ Unit/making it/step1/IF_Stage.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2023/11/27 23:04:58
-// Design Name: 
-// Module Name: IF_Stage
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 // synthesis, Implementation 통과
 
 module IF_Stage(
