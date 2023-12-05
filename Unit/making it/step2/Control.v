9bec864439df13b3fa6d74b4ba62450e7aaf5e6c
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-05 16:01:13 +0900

    Update Control.v

--- Unit/making it/step2/Control.v
+++ Unit/making it/step2/Control.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2023/12/04 12:01:48
-// Design Name: 
-// Module Name: Control
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Control(
     input wire hazard_detected,
     input wire [5:0] opcode,
