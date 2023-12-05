a34b28f415ec94599e2e29b65e043a79189a91cd
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-05 16:01:44 +0900

    Update Adder.v

--- Unit/making it/step1/Adder.v
+++ Unit/making it/step1/Adder.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2023/11/28 00:47:03
-// Design Name: 
-// Module Name: Adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Adder(
     input wire [31:0] operandA, // 피연산자 A
     input wire [31:0] operandB, // 피연산자 B
