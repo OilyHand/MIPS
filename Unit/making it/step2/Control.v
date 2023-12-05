617d091b17eb24e0376283164d15e1e7599611c9
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-05 16:52:36 +0900

    Update Control.v

--- Unit/making it/step2/Control.v
+++ Unit/making it/step2/Control.v
@@ -1,27 +1,46 @@
 module Control(
     input wire hazard_detected,
     input wire [5:0] opcode,
-    output reg ALUSrc, RegDst,
     output reg [1:0] ALUOp,                   // stage 3에서 사용
+    output reg ALUSrc, RegDst,                  
     output reg Branch, MemRead, MemWrite,     // stage 4에서 사용
     output reg RegWrite, MemtoReg, PCSrc);    // stage 5 이후에서 사용
 
-
+    
 always @* begin
-    if (hazard_detected == 0) begin
-        {ALUSrc, RegDst, ALUOp, Branch, MemRead, MemWrite, RegWrite, MemtoReg, PCSrc} <= 0;
+    if (hazard_detected == 0) begin   
+        ALUSrc <= 1'b0;
+        RegDst <= 1'b0;
+        ALUOp <= 1'b0;
+        Branch <= 1'b0;
+        MemRead <= 1'b0;
+        MemWrite <= 1'b0;
+        RegWrite <= 1'b0;
+        MemtoReg <= 1'b0;
+        PCSrc <= 1'b0;
+        
         if (opcode == 6'b100011) begin // LW instruction
-            {ALUSrc, MemRead, RegWrite, MemtoReg} <= 1; end
-        else if (opcode == 6'b101011) begin // SW instrtuction
-            {ALUSrc, MemWrite} <= 1; end
-        else if (opcode == 6'b000100) begin // BEQ instruction
-            {ALUOp, Branch} <= 1; end
-        else if (opcode == 6'b000000) begin // R-type instruction
-             ALUOp <= 2;
-            {RegDst, RegWrite} <= 1; end
+            ALUSrc <= 1'b1;
+            MemRead <= 1'b1;
+            RegWrite <= 1'b1;
+            MemtoReg <= 1'b1; end 
+            
+        else if (opcode == 6'b101011) begin // SW instrtuction       
+            ALUSrc <= 1'b1;
+            MemWrite <= 1; end
+            
+        else if (opcode == 6'b000100) begin // BEQ instruction      
+            ALUOp <= 1'b1;
+            Branch <= 1'b1; end
+            
+        else if (opcode == 6'b000000) begin // R-type instruction       
+            ALUOp <= 2'b10;
+            RegDst <= 1'b1;
+            RegWrite <= 1'b1; end
+            
     end
     else begin
-    
     end
+    
 end
 endmodule
