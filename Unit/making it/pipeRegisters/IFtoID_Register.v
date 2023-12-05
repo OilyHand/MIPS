117e93c54a39c354dc278a06fdf30726dad8ffba
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-05 16:02:09 +0900

    Update IFtoID_Register.v

--- Unit/making it/pipeRegisters/IFtoID_Register.v
+++ Unit/making it/pipeRegisters/IFtoID_Register.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2023/12/01 02:09:26
-// Design Name: 
-// Module Name: IFID
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module IFtoID_Register (input wire clk, rst,
   input wire [31:0] IF_PC, IF_inst,
   output reg [31:0] ID_PC, ID_inst);
