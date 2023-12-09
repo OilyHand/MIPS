a59979d825e0d2f4b4a6c2bfed2b15d628dd85d0
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-10 03:53:29 +0900

    Update ProgramCounter.v

--- Unit/making it/step1/ProgramCounter.v
+++ Unit/making it/step1/ProgramCounter.v
@@ -1,12 +1,6 @@
 module ProgramCounter(
     input wire clk,        // 클락 신호
     
-    input wire [31:0] branchAddress, // branch 목적지 주소
-    input wire branch,     // branch 여부
-    
-    input wire [31:0] jumpAddress,   // jump 목적지 주소
-    input wire jump,       // jump 여부
-    
     input wire [31:0] PCWriteValue,  // PC에 쓰여질 값
     input wire PCWrite,    // Control unit
     
@@ -14,13 +8,7 @@ module ProgramCounter(
 );
 
     always @(posedge clk) begin
-        if (jump)          // jump 활성화
-            pc <= jumpAddress;  // jump 목적지 주소 넣기
-        
-        else if (branch)   // branch 활성화
-            pc <= pc + branchAddress;  // branch 목적지 주소로 이동
-        
-        else if (PCWrite)  // PCwrite Control unit이 활성화되면
+        if (PCWrite)  // PCwrite Control unit이 활성화되면
             pc <= PCWriteValue;  // 그 값으로 pc 덮어쓰기
     end
 
