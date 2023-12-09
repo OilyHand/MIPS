a663ca25eeb172ae00f71dd5007f1375dc5975cf
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-10 03:55:32 +0900

    Update Control.v

--- Unit/making it/Control.v
+++ Unit/making it/Control.v
@@ -1,46 +1,47 @@
 module Control(
     input wire hazard_detected,
     input wire [5:0] opcode,
-    output reg [1:0] ALUOp,                   // stage 3에서 사용
+    output reg [1:0] ALUOp,
     output reg ALUSrc, RegDst,                  
-    output reg Branch, MemRead, MemWrite,     // stage 4에서 사용
-    output reg RegWrite, MemtoReg, PCSrc);    // stage 5 이후에서 사용
-
+    output reg Branch, MemRead, MemWrite,
+    output reg RegWrite, MemtoReg, PCSrc);
     
-always @* begin
-    if (hazard_detected == 0) begin   
-        ALUSrc <= 1'b0;
-        RegDst <= 1'b0;
-        ALUOp <= 1'b0;
-        Branch <= 1'b0;
-        MemRead <= 1'b0;
-        MemWrite <= 1'b0;
-        RegWrite <= 1'b0;
-        MemtoReg <= 1'b0;
-        PCSrc <= 1'b0;
-        
-        if (opcode == 6'b100011) begin // LW instruction
-            ALUSrc <= 1'b1;
-            MemRead <= 1'b1;
-            RegWrite <= 1'b1;
-            MemtoReg <= 1'b1; end 
-            
-        else if (opcode == 6'b101011) begin // SW instrtuction       
-            ALUSrc <= 1'b1;
-            MemWrite <= 1'b1; end
-            
-        else if (opcode == 6'b000100) begin // BEQ instruction      
-            ALUOp <= 1'b1;
-            Branch <= 1'b1; end
-            
-        else if (opcode == 6'b000000) begin // R-type instruction       
-            ALUOp <= 2'b10;
-            RegDst <= 1'b1;
-            RegWrite <= 1'b1; end
-            
+    always @* begin
+      ALUSrc = 1'b0;
+      RegDst = 1'b0;
+      ALUOp = 2'b00;
+      Branch = 1'b0;
+      MemRead = 1'b0;
+      MemWrite = 1'b0;
+      RegWrite = 1'b0;
+      MemtoReg = 1'b0;
+      PCSrc = 1'b0;
+      
+      if(!hazard_detected) begin
+      case(opcode)
+        6'b100011: begin // LW instruction
+            ALUSrc = 1'b1;
+            MemRead = 1'b1;
+            RegWrite = 1'b1;
+            MemtoReg = 1'b1; 
+          end
+        6'b101011: begin // SW instrtuction       
+            ALUSrc = 1'b1;
+            MemWrite = 1'b1;
+          end
+        6'b000100: begin // BEQ instruction      
+            ALUOp = 1'b1;
+            Branch = 1'b1;
+        end
+        6'b000000: begin // R-type instruction       
+            ALUOp = 2'b10;
+            RegDst = 1'b1;
+            RegWrite = 1'b1;
+        end
+ /*       6'b000010: begin // jump instruction
+        end*/
+        default: ;    
+      endcase
     end
-    else begin
     end
-    
-end
 endmodule
