b897d56c8ca2470531057a97fbcb8c062c5998a9
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-12 20:44:55 +0900

    Update IFtoID_Register.v

--- Unit/making it/pipeRegisters/IFtoID_Register.v
+++ Unit/making it/pipeRegisters/IFtoID_Register.v
@@ -1,17 +1,34 @@
 module IFtoID_Register (
     input wire clk, rst,
     input wire [31:0] IF_PC, IF_inst,
+    input wire IF_ID_Write,
+    
     output reg [31:0] ID_PC, ID_inst);
  
  
-  always @ (posedge clk) begin
+    reg [31:0] prev_IF_PC;      //이전 값 저장
+    reg [31:0] prev_IF_inst;    //이전 값 저장
+ 
+ 
+  always @ (posedge clk or posedge rst) begin
     if (rst) begin
         ID_PC <= 0;
         ID_inst <= 0;
+        prev_IF_PC <= 0;      //이전 값 저장
+        prev_IF_inst <= 0;
     end
+    
     else begin
-        ID_PC <= IF_PC;
-        ID_inst <= IF_inst;
+        if(IF_ID_Write) begin
+            ID_PC <= IF_PC;
+            ID_inst <= IF_inst;
+        end
+        else begin
+            ID_PC <= prev_IF_PC;
+            ID_inst <= prev_IF_inst;
+        end
+      prev_IF_PC <= IF_PC;
+      prev_IF_inst <= IF_inst;
     end
   end
 endmodule
