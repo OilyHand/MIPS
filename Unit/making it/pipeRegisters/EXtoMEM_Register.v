43869c3b4f8b1b97ebb9745c32d1b871e8da7124
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-15 11:21:33 +0900

    Update EXtoMEM_Register.v

--- Unit/making it/pipeRegisters/EXtoMEM_Register.v
+++ Unit/making it/pipeRegisters/EXtoMEM_Register.v
@@ -4,7 +4,7 @@ module EXtoMEM_Register(
     // datapath input
     input wire EX_zero,
     input wire [31:0] EX_ALUresult,
-    input wire [4:0] EX_Rt,
+    input wire [31:0] EX_ReadData2,
     input wire [31:0] EX_Branch_Addr,
     input wire [4:0] EX_RegDest,
  //   input wire [31:0] EX_Jump_address,
@@ -21,7 +21,7 @@ module EXtoMEM_Register(
     // datapath output
     output reg EXtoMEM_zero,
     output reg [31:0] EXtoMEM_ALUresult,
-    output reg [4:0] EXtoMEM_Rt,
+    output reg [31:0] EXtoMEM_ReadData2,
     output reg [31:0] EXtoMEM_Branch_Addr,
 //    output reg [31:0] EXtoMEM_Jump_address,
     output reg [4:0] EXtoMEM_RegDest,
@@ -35,21 +35,39 @@ module EXtoMEM_Register(
     output reg MEM_RegWrite
 );
 
-    always @(posedge clk) begin
-        // datapath
-        EXtoMEM_zero <= EX_zero;
-        EXtoMEM_ALUresult <= EX_ALUresult;
-        EXtoMEM_Rt <= EX_Rt;
-        EXtoMEM_Branch_Addr <= EX_Branch_Addr;
-        EXtoMEM_RegDest <= EX_RegDest;
-//        EXtoMEM_Jump_address <= EX_Jump_address;
-
-        // control
-        MEM_Branch <= EX_Branch;
-        MEM_MemRead <= EX_MemRead;
-        MEM_MemWrite <= EX_MemWrite;
-        MEM_MemtoReg <= EX_MemtoReg;
-        MEM_RegWrite <= EX_RegWrite;
+    always @(posedge clk or posedge rst) begin
+        if(rst) begin
+            // datapath
+            EXtoMEM_zero <= 0;
+            EXtoMEM_ALUresult <= 0;
+            EXtoMEM_ReadData2 <= 0;
+            EXtoMEM_Branch_Addr <= 0;
+            EXtoMEM_RegDest <= 0;
+    //        EXtoMEM_Jump_address <= EX_Jump_address;
+    
+            // control
+            MEM_Branch <= 0;
+            MEM_MemRead <= 0;
+            MEM_MemWrite <= 0;
+            MEM_MemtoReg <= 0;
+            MEM_RegWrite <= 0;
+        end
+        else begin
+            // datapath
+            EXtoMEM_zero <= EX_zero;
+            EXtoMEM_ALUresult <= EX_ALUresult;
+            EXtoMEM_ReadData2 <= EX_ReadData2;
+            EXtoMEM_Branch_Addr <= EX_Branch_Addr;
+            EXtoMEM_RegDest <= EX_RegDest;
+            // EXtoMEM_Jump_address <= EX_Jump_address;
+    
+            // control
+            MEM_Branch <= EX_Branch;
+            MEM_MemRead <= EX_MemRead;
+            MEM_MemWrite <= EX_MemWrite;
+            MEM_MemtoReg <= EX_MemtoReg;
+            MEM_RegWrite <= EX_RegWrite;
+        end
     end
 
 endmodule
