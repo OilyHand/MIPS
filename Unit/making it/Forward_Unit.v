ae3c8cef18864cceb95cbf614cb0dd6bb581d046
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-17 01:38:48 +0900

    Update Forward_Unit.v

--- Unit/making it/Forward_Unit.v
+++ Unit/making it/Forward_Unit.v
@@ -20,12 +20,11 @@ module Forward_Unit(
         
         // MEM hazard
         if(MEM_WB_RegWrite && (MEM_WB_Rd != 32'd0)) begin
-            if(!(EX_MEM_RegWrite && (EX_MEM_Rd != 32'd0) && (EX_MEM_Rd == ID_EX_Rs))
-                && MEM_WB_Rd == ID_EX_Rs)
+            if((MEM_WB_Rd == ID_EX_Rs) && !((EX_MEM_RegWrite) && (EX_MEM_Rd != 32'd0) && (EX_MEM_Rd == ID_EX_Rs)))
                 ForwardA = 2'b01;
-            if(!EX_MEM_RegWrite && (EX_MEM_Rd != 32'd0) && (EX_MEM_Rd == ID_EX_Rt)
-                && MEM_WB_Rd == ID_EX_Rs)
+            if((MEM_WB_Rd == ID_EX_Rt) && !((EX_MEM_RegWrite) && (EX_MEM_Rd != 32'd0) && (EX_MEM_Rd == ID_EX_Rt)))
                 ForwardB = 2'b01;
         end
+        
     end
 endmodule
