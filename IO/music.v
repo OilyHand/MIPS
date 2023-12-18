6aa2140b6638d9722962d6d486398ce35a9e1b6f
Hingguripongpong <150646567+Hingguripongpong@users.noreply.github.com>
2023-12-19 08:28:32 +0900

    Update music.v

--- IO/music.v
+++ IO/music.v
@@ -1,14 +1,16 @@
-module music(clk, rst, speaker);
+`timescale 1ns / 1ps
+
+module audio(clk, rst, speaker);
   input clk, rst;
   output speaker;
 
-  reg [14:0] counter;
+  reg [16:0] counter; //113632는 00011011101111100000 곧 2진수 17비트
   reg speaker;
 
   always @(posedge clk) begin
-    if (counter == 28408) begin 
-        //A는 440Hz이므로 주파수 = 클럭 주기 / 카운터 값 이므로 56816로 나온다.
-        //duty cycle을 50% 곧 켜져 있는 시간과 꺼져 있는 시간을 50%로 만들기 위해 다시 반으로 나눈다. 28408
+    if (counter == 113632) begin 
+        //A는 440Hz이므로 주파수 = 클럭 주기 / 카운터 값 이므로 카운터 값은 227264로 나온다.
+        //duty cycle을 50% 곧 켜져 있는 시간과 꺼져 있는 시간을 50%로 만들기 위해 다시 반으로 나눈다. 113632
       counter <= 0;
       if (rst) begin
         speaker <= ~speaker;
